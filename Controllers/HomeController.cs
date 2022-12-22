f6eaff60528fd45d91bf3db1d82339aae7b2d4d3
Julien Charbonneau <juliencharbonneauqc@gmail.com>
2022-12-22 11:21:55 -0500

    ouput customer building info but is messy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,10 +16,9 @@ namespace Rocket_Elevators_Customer_Portal.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public string getCustomerId()
         {
-            //test to call api with end point tested on postman
-            string api_url = String.Format("https://localhost:7047/api/Column/GetColumnStatusById?id=16");
+            string api_url = String.Format("https://localhost:7047/api/Customer/GetCustomerIdByEmail?input_email=werner_flatley@schiller.com");
             WebRequest requestObject = WebRequest.Create(api_url);
             requestObject.Method = "GET";
             HttpWebResponse responseObject = null;
@@ -32,9 +31,37 @@ namespace Rocket_Elevators_Customer_Portal.Controllers
                 result_api = sr.ReadToEnd();
                 sr.Close();
             }
+            return result_api;
+        }
 
-            var a = 1;
-            ViewBag.Message = result_api;
+        public string getBuildingByCustomerId (long id)
+        {
+            string api_url = String.Format("https://localhost:7047/api/Building/GetBuildingByCustomerId?id=1");
+            WebRequest requestObject = WebRequest.Create(api_url);
+            requestObject.Method = "GET";
+            HttpWebResponse responseObject = null;
+            responseObject = (HttpWebResponse)requestObject.GetResponse();
+
+            string result_api = null;
+            using (Stream stream = responseObject.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                result_api = sr.ReadToEnd();
+                sr.Close();
+            }
+            return result_api;
+        }
+
+        public IActionResult Index()
+        {
+
+           
+
+
+            
+            ViewBag.CustomerId = getCustomerId();
+            ViewBag.CustomerBuilding = getBuildingByCustomerId(1);
+
             return View();
         }
 
